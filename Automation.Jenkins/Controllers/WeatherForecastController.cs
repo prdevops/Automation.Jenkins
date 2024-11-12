0bf47df7f38c48a840ab679a42cacfb3695653c6
mushfig-agahyev-developer <mushfig.aghayev.webdeveloper@gmail.com>
2024-11-13 01:53:18 +0400

    r

--- Automation.Jenkins/Controllers/WeatherForecastController.cs
+++ Automation.Jenkins/Controllers/WeatherForecastController.cs
@@ -31,6 +31,7 @@ namespace Automation.Jenkins.Controllers
         }
     }
 
+
     public record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
     {
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
