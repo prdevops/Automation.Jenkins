46b9a70bd8b2738d53a3036ece25426ed1ec7271
mushfig-aghayev-developer <webprogrammer.readonly@gmail.com>
2025-03-28 18:17:41 +0400

    ok

--- Automation.Jenkins/start.sh
+++ Automation.Jenkins/start.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 # Stop any running containers and remove them with docker-compose
-docker-compose down
+#docker-compose down
 
 # Rebuild, recreate, and start the container with docker-compose in detached mode
-docker-compose up --force-recreate --build -d
+#docker-compose up --force-recreate --build -d
 
 # Build the Docker image and tag it for pushing to Docker Hub
-docker build -t mushudevops/automationwithjenkins ./Automation.Jenkins/.
+#docker build -t mushudevops/automationwithjenkins ./Automation.Jenkins/.
 
 # Push the newly built image to Docker Hub
-docker push mushudevops/automationwithjenkins
+#docker push mushudevops/automationwithjenkins
 
+#***********************************************************
 # Optional: Run a separate container from the pushed image (if needed)
 # Uncomment the line below if you want to run an additional container independently
 # docker run -d -p 44301:80 --name c_automationwithjenkins mushudevops/automationwithjenkins
